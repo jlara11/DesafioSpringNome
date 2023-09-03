82a75f0a34e9c383a81b2514c8d6af7c9957eb71
jlara11 <julyana.pd2@gmail.com>
2023-09-03 20:01:06 -0300

    DesafioAceito

---
+++ src/main/java/br/com/fatecdiadema/ProjetoDoZeroApllication.java
@@ -0,0 +1,13 @@
+package br.com.fatecdiadema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDoZeroApllication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDoZeroApllication.class, args);
+	}
+
+}
